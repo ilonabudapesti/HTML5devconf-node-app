1d2771940d2e5190ca44777620dae963702eade6
Monthy Python <monthy.python@supeek.com>
2014-05-20 12:56:55 -0700

    Add app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+/*var http = require('http'); //add the http module
+var myServer = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write("<b>Hello</b> World");
+  response.end();
+}); //create a server
+
+myServer.listen(3000);
+
+console.log("Go to http://localhost:3000 on your browser");
+*/
+
+
+// better version
+
+// third version
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('<H1>Hello</H1> Express');
+});
+
+app.get('/me', function(req, res) {
+  res.send('@planetoftheweb');
+});
+
+app.get('/who/:name?', function(req, res) {
+  var name = req.params.name;
+  res.send(name + ' was here');
+});
+
+app.get('/who/:name?/:title?', function(req, res) {
+  var name = req.params.name;
+  var title = req.params.title;
+  res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+});
+
+app.get('*', function(req, res) {
+  res.send('Bad Route');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
